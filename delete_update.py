817349f8f6fac74d56ac87985ac752f4ddf64f25
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-18 01:26:40 +0200

    created a mini project

--- delete_update.py
+++ delete_update.py
@@ -5,7 +5,28 @@ import requests
 # Simulate updating an event title
 
 
-delete_data = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
+def delete_event(event_id):
+    url = f"https://jsonplaceholder.typicode.com/posts/{event_id}"
+    response = requests.delete(url)
+    if response.status_code == 200:  # 200 = success
+        print(f"Event {event_id} deleted successfully!")
+    else:
+        print(f"Failed to delete event {event_id}. Status code:", response.status_code)
+delete_event(1)
 
-print(delete_data.status_code)
-print(delete_data.json())
+
+# Update event
+def update_event(event_id, new_title):
+    url = f"https://jsonplaceholder.typicode.com/posts/{event_id}"
+    data = {
+        "title": new_title
+    }
+    response = requests.put(url, json=data)
+    if response.status_code == 200:
+        print(f"Event {event_id} updated successfully!")
+        print(response.json())
+    else:
+        print(f"Failed to update event {event_id}. Status code:", response.status_code)
+
+update_event(1, "Updated Meeting with Team")
+update_event(2, "Updated Gym Time")
