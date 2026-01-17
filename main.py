a2084bd0706598f0202f287eb61917c2c6e1c939
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-18 00:04:50 +0200

    getting into an api explanations

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 
 # Here I will be learning how to use an API and how to intergrate it to my programmes
 
+''' 
+    An API is like a waiter 🍽️
+    - Your program = customer
+    - API = waiter
+    - Server (Google Calendar) = kitchen
+
+    Me:
+        - “Give me my events”
+
+    API:
+        - “Here they are (in JSON format)”
+
+
+'''
+
+
